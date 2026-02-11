91ab47ffc37be24d6e6e983972a2fd3b28e36264
Tyler Murray <tylerm@allenai.org>
2026-02-10 21:33:34 -0800

    try custom docker location

--- src/olmo_eval/harness/sandbox/executor.py
+++ src/olmo_eval/harness/sandbox/executor.py
@@ -120,6 +120,7 @@ class SandboxExecutor:
                     image=self.config.image,
                     container_runtime="docker",
                     startup_timeout=self.config.startup_timeout,
+                    docker_internal_host=self.config.docker_internal_host,
                 )
 
             case SandboxMode.LOCAL:
