e608c2e2280fc033343d3eaaa843afb34b146eee
Tyler Murray <tylerm@allenai.org>
2026-02-25 11:20:56 -0800

    GCP cred stuff for registry

--- src/olmo_eval/cli/beaker/launch.py
+++ src/olmo_eval/cli/beaker/launch.py
@@ -111,6 +111,12 @@ from olmo_eval.common.constants.infrastructure import BEAKER_RESULT_DIR, BEAKER_
     help="Inject GCS credentials for gs:// model access. Auto-detected from gs:// model paths.",
 )
 @click.option(
+    "--use-gcp-creds/--no-use-gcp-creds",
+    "gcp_secret",
+    default=None,
+    help="Inject GOOGLE_APPLICATION_CREDENTIALS from Beaker secret ({user}_...)",
+)
+@click.option(
     "--s3-bucket",
     help="S3 bucket for storing evaluation results (required for S3 uploads)",
 )
@@ -251,6 +257,7 @@ def launch(
     follow: bool,
     aws_credentials: bool | None,
     gcs_credentials: bool | None,
+    gcp_secret: bool | None,
     s3_bucket: str | None,
     s3_prefix: str | None,
     s3_endpoint_url: str | None,
@@ -361,6 +368,7 @@ def launch(
         "harness_overrides": harness_overrides,
         "uv_cache_dir": uv_cache_dir,
         "secret_env_overrides": secret_env_overrides,
+        "gcp_secret": gcp_secret,
     }
 
     # Handle external evaluations mode
@@ -402,6 +410,7 @@ def launch(
             follow=follow,
             aws_credentials=aws_credentials,
             gcs_credentials=gcs_credentials,
+            gcp_secret=gcp_secret,
             s3_bucket=s3_bucket,
             s3_prefix=s3_prefix,
             s3_region=s3_region,
@@ -568,6 +577,7 @@ def launch(
         inject_gcs,
         enable_sandbox=harness_needs_sandbox,
         secret_env_overrides=launch_config.secret_env_overrides,
+        inject_gcp_secret=launch_config.inject_gcp_secret,
     )
 
     job_configs = []
@@ -979,6 +989,7 @@ def _launch_external_evals(
     follow: bool,
     aws_credentials: bool | None,
     gcs_credentials: bool | None,
+    gcp_secret: bool | None,
     s3_bucket: str | None,
     s3_prefix: str | None,
     s3_region: str,
@@ -1156,6 +1167,7 @@ def _launch_external_evals(
             env_secrets=env_secrets,
             inject_aws_credentials=inject_aws,
             inject_gcs_credentials=inject_gcs,
+            inject_gcp_secret=gcp_secret or False,
             eval_args=eval_args,
             provider_kwargs=provider_kwargs,
             uv_cache_dir=uv_cache_dir,
