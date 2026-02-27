63e1ebab1e6cfa86f4f324394df2ee767016c288
Ronan Le Bras <ronanlb@allenai.org>
2026-02-26 16:06:52 -0800

    testing with vllm 0.11.0

--- src/olmo_eval/common/constants/models.py
+++ src/olmo_eval/common/constants/models.py
@@ -40,8 +40,9 @@ def get_model_presets() -> dict[str, ProviderConfig]:
             kind=ProviderKind.VLLM,
             model="allenai/Olmo-3-1025-7B",
             trust_remote_code=True,
-            max_model_len=5000,
+            max_model_len=4096,
             revision="stage2-step47684",
+            package="vllm==0.11.0",
             kwargs={"gpu_memory_utilization": 0.7, "add_bos_token": False},
         ),
         "olmo-2-7b": ProviderConfig(
