de58027a5d11e9e38ecdfb04e95820ea1b34c637
Tyler Murray <tylerm@allenai.org>
2026-02-27 12:22:09 -0800

    Fix tests

--- tests/providers/test_vllm_server_startup.py
+++ tests/providers/test_vllm_server_startup.py
@@ -127,4 +127,4 @@ class TestVLLMServerStartupTimeout:
             )
 
             url = server.start()
-            assert url == "http://localhost:8000/v1"
+            assert url in ("http://localhost:8000/v1", "http://127.0.0.1:8000/v1")
