edd7049c51aa21197ccd4fdaf4b8c4fd598e9c6a
Tyler Murray <tylerm@allenai.org>
2026-05-12 12:52:16 -0700

    Make a post train math suite

--- src/olmo_eval/evals/suites/math.py
+++ src/olmo_eval/evals/suites/math.py
@@ -6,3 +6,17 @@ make_suite(
     tuple(f"minerva_math_{t}" for t in MATH_SUBSETS),
     aggregation=AggregationStrategy.AVERAGE,
 )
+
+make_suite(
+    "math:posttrain:dev",
+    (
+        "aime_2025",
+        "aime_2026",
+        "hmmt_nov_2025",
+        "hmmt_feb_2026",
+    ),
+    aggregation=AggregationStrategy.AVERAGE,
+    description=(
+        "Dev set for post-training math experiments: AIME 2025/2026 and HMMT Nov 2025 / Feb 2026."
+    ),
+)
