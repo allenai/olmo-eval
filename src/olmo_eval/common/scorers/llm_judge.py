dc14e9393ef8d0d98259eb9ba1582274282c6944
mgmorgan23 <maliam@allenai.org>
2026-06-10 10:07:34 -0700

    longer context

--- src/olmo_eval/common/scorers/llm_judge.py
+++ src/olmo_eval/common/scorers/llm_judge.py
@@ -546,7 +546,7 @@ class SafetyScorer(LLMJudgeScorer):
                     SamplingParams(
                         temperature=0.0,
                         max_tokens=128,
-                        truncate_prompt_tokens=3600,
+                        truncate_prompt_tokens=32600,
                         truncation_side="right",
                     ),
                     request_type=self.judge_request_type,
